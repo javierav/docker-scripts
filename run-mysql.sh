e29b5070fdb082cac323c7785da9288d0901fbca
Javier Aranda <javier.aranda.varo@gmail.com>
2022-07-05 18:26:19 +0200

    Add Oracle MySQL

--- run-mysql.sh
+++ run-mysql.sh
@@ -7,4 +7,4 @@ docker run \
   -p 3306:3306 \
   --mount 'type=volume,source=mysql_storage,target=/var/lib/mysql,volume-driver=local' \
   -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-  mysql/mysql-server:8.0
+  mysql:8.0.29-oracle
